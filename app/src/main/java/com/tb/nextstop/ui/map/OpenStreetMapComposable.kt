6de926a60d147672ff446456a2fd6af5374c7940
Taj Brar <taj.brar3047@gmail.com>
2025-04-21 21:12:54 -0500

    Update stop icons and set bounds for Map screen

--- app/src/main/java/com/tb/nextstop/ui/map/OpenStreetMapComposable.kt
+++ app/src/main/java/com/tb/nextstop/ui/map/OpenStreetMapComposable.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.tb.nextstop.R
 import com.tb.nextstop.data.Stop
 import com.tb.nextstop.utils.checkAndRequestLocationPermission
 import org.osmdroid.util.GeoPoint
@@ -12,7 +14,13 @@ import org.osmdroid.views.overlay.Marker
 
 const val WPG_LAT = 49.8954
 const val WPG_LON = -97.1385
+
 private const val DEFAULT_ZOOM = 18.0
+private const val MIN_ZOOM = 13.0
+private const val N_LAT_LIMIT = 50.0
+private const val S_LAT_LIMIT = 49.7
+private const val W_LON_LIMIT = -97.4
+private const val E_LON_LIMIT = -96.9
 
 @Composable
 fun OpenStreetMapComposable(
@@ -32,11 +40,16 @@ fun OpenStreetMapComposable(
                     context,
                     android.preference.PreferenceManager.getDefaultSharedPreferences(context)
                 )
+
                 setTileSource(org.osmdroid.tileprovider.tilesource.TileSourceFactory.MAPNIK)
                 setMultiTouchControls(true)
                 zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
                 controller.setZoom(DEFAULT_ZOOM)
                 controller.setCenter(GeoPoint(WPG_LAT, WPG_LON))
+
+                setScrollableAreaLimitLatitude(N_LAT_LIMIT, S_LAT_LIMIT, 0)
+                setScrollableAreaLimitLongitude(W_LON_LIMIT, E_LON_LIMIT, 0)
+                minZoomLevel = MIN_ZOOM
             }
 
             val locationOverlay = org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay(
@@ -51,6 +64,12 @@ fun OpenStreetMapComposable(
             mapView
         },
         update = { mapView ->
+            val busStopIcon = ContextCompat.getDrawable(
+                mapView.context,
+                R.drawable.bus
+            )?.mutate()
+            busStopIcon?.setTint(ContextCompat.getColor(mapView.context, R.color.teal_700))
+
             val markers: List<Marker?> = stops.mapNotNull {
                 val lat = it.centre.geographic.latitude.toDoubleOrNull()
                 val lon = it.centre.geographic.longitude.toDoubleOrNull()
@@ -59,6 +78,8 @@ fun OpenStreetMapComposable(
                     val marker = Marker(mapView)
                     marker.position = GeoPoint(lat, lon)
                     marker.title = it.name
+                    marker.icon = busStopIcon
+
                     marker
                 } else {
                     null
